02b047b2df42dc9fd63cc82cce8f2d378d8f45ef
AndrewFaugno <andrewfaugno825@gmail.com>
2022-10-14 16:44:51 -0400

    updated contact form with functionality

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,11 +2,12 @@ import { useState } from 'react';
 import { validateEmail } from '../../utils/helpers';
 
 function Contact() {
-    const [contactInfo, setContactInfo ] = useState({Name: '', Email: '', Message: ''});
-    const [ errorMessage, setErrorMessage ] = useState('');
+    const [contactInfo, setContactInfo] = useState({ name: '', email: '', message: '' });
+    const [errorMessage, setErrorMessage] = useState('');
+    const [formComplete, setFormComplete] = useState(false)
 
     function handleChange(e) {
-        if (e.target.name === 'Email') {
+        if (e.target.name === 'email') {
             const valid = validateEmail(e.target.value);
 
             if (!valid) {
@@ -23,40 +24,48 @@ function Contact() {
         }
 
         if (!errorMessage) {
-            setContactInfo({...contactInfo, [e.target.name]: e.target.value});
+            setContactInfo({ ...contactInfo, [e.target.name]: e.target.value });
+        }
+
+        if (contactInfo.name && contactInfo.email && contactInfo.message) {
+            setFormComplete(true);
+        } else {
+            setFormComplete(false)
         }
     }
 
     function handleSubmit(e) {
-        e.preventDefault();
-        setErrorMessage('Message has been sent! (jk)')
+        // e.preventDefault();
+        if (!formComplete) {
+            e.preventDefault();
+        }
     }
 
     return (
         <div className="w-50 m-auto text-center pt-3 form-container">
             <div className="w-full">
-                <h3 className='dynapuff'>Contact Me</h3>
+                <h3 className="dynapuff">Contact Me</h3>
             </div>
-            <form id="contact-form" onSubmit={handleSubmit}>
+            <form action="https://formsubmit.co/andrewfaugno825@gmail.com" method="POST" id="contact-form" onSubmit={handleSubmit}>
                 <div className="form-group text-left quicksand">
-                    <label htmlFor="Name">Name:</label>
-                    <input type="text" name="Name" className="form-control" onBlur={handleChange}/>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" name="name" className="form-control" id="nameTxt" onChange={handleChange} />
                 </div>
                 <div className="form-group text-left quicksand">
-                    <label htmlFor="Email">Email:</label>
-                    <input type="email" name="Email" className="form-control" onBlur={handleChange}/>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" name="email" className="form-control" id="emailTxt" onChange={handleChange} />
                 </div>
                 <div className="form-group text-left quicksand">
-                    <label htmlFor="Message">Message:</label>
-                    <textarea rows="5" name="Message" type="message" className="form-control" onBlur={handleChange} />
+                    <label htmlFor="message">Message:</label>
+                    <textarea rows="5" name="message" type="message" className="form-control" id="messageTxt" onChange={handleChange} />
                 </div>
-                <button type="submit" className="btn btn-danger btn-lg btn-block quicksand">Submit</button>
+                <button type="submit" className="btn btn-danger btn-lg btn-block quicksand">
+                    Send
+                </button>
             </form>
-            <h4 className='pt-2 quicksand'>
-                {errorMessage}
-            </h4>
+            <h4 className="pt-2 quicksand">{errorMessage}</h4>
         </div>
-    )
+    );
 }
 
 export default Contact;
